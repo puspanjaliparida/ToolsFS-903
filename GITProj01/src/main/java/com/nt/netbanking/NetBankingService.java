f9dc7e0cbde61d51f700b25380c42174900e09f6
puspanjaliparida <paridapuspanjali22@gmail.com>
2024-11-11 12:40:09 +0530

    branch2 enhancement tl

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -6,4 +6,12 @@ public class NetBankingService {
       public String OpenAccount(String aadhar,double initialamount,String type) {
     	  return"account is opened"+new Random().nextLong(68888888888888888L);		
       }
+      
+      public String withdraw(long acno,double amount) {
+    	  return amount+"is withdrawn from the account number"+acno;
+      }
+      
+      public String deposit(long acno,double amount) {
+    	  return amount+"is deposited into  the account number"+acno;
+      }
 }
