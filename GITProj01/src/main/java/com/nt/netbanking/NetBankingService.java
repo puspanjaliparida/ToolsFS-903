a0a4a6b890407217cad9969d8b2377cac209a46f
puspanjaliparida <paridapuspanjali22@gmail.com>
2024-11-11 21:31:20 +0530

    hjjk

--- GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/netbanking/NetBankingService.java
@@ -6,4 +6,7 @@ public class NetBankingService {
       public String OpenAccount(String aadhar,double initialamount,String type) {
     	  return"account is opened"+new Random().nextLong(68888888888888888L);		
       }
+      public String withdraw(long acno,double amount) {
+    	  return amount+"is withdrawn from the account number::"+acno;
+      }
 }
